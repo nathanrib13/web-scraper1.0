98a6204cd2bcef90a6ab3d0ff08198ad2bfc7aee
nathanrib13 <nathan.ribeiros7@gmail.com>
2024-01-24 20:27:42 -0300

    api created

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
+const express = require("express")
 
+const app = express()
+const PORT = 3000
 const url = "https://globo.com/"
 
-async function main (){
+app.get("/posts", async (req, res)=>{
+    try {
+        const posts = await scrapePosts()
+        res.status(200).json({posts})
+    } catch (error) {
+        res.status(500).json({messageL: "Error fetching posts"})
+    }
+})
+
+async function scrapePosts (){
     const response =  await axios(url)
     const html = response.data
     const $ = cheerio.load(html)
@@ -13,7 +25,7 @@ async function main (){
         const postTitle = $(this).find(".post__title").text()
         posts.push({url, postTitle})
     })
-    console.log({posts})
+    return posts
 }
 
-main()  
+app.listen(PORT,() =>{console.log(`server is running at port ${PORT}`)})
