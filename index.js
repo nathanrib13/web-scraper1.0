9cb155aa99ddbb3150bdca45ded4cae83b15c6d4
nathanrib13 <nathan.ribeiros7@gmail.com>
2024-01-31 22:54:02 -0300

    getting products links from mercado livre

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 const puppeteer = require('puppeteer')
 
-const url = "https://gshow.globo.com/realities/bbb"
+const url = "https://www.mercadolivre.com.br/"
+const searchFor = "mackbook"
 
 async function main(){
-    const browser = await puppeteer.launch({
-        headless: false
-    });
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage()
+    
     await page.goto(url)
-
-    await page.evaluate(() =>{
-        const posts = Array.from(document.querySelectorAll(".post-item"))
-
-        const data = posts.map((post)=>{
-            url: post.querySelector('post-materia-text')?.getAttribute('.href')
-            title: post.querySelector('.post-materia-text__title')?.textContent
-            description: post.querySelector('.post-materia-text__description')?.textContent
-        })
-
-        return data
-    })
-
-
-
-
+    await page.waitForSelector("#cb1-edit")
+    
+    await page.type("#cb1-edit", searchFor)
+    
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click(".nav-search-btn")
+    ])
+    
+    const links = await page.$$eval('.ui-search-item__group > a', el => el.map(link => link.href))
+    console.log(links)
+    await page.waitForTimeout(3000)
     await browser.close()
 }
 main()
